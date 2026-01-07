92a422849cc35ebbf3797969118afc32f3d129fd
Wilpersan <38378542+wilpersan@users.noreply.github.com>
2026-01-07 02:17:56 +0000

    Ajuste da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olá. Te apresento minha primeira página Publicada na WEB!</h1>
+    return <h1>Olá! Já te disse o quanto você é importante para mim! Se não, você é extremamente importante!</h1>
 }
 
 export default Home;
