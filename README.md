9096c27c250d3496593a887172ebb3a32e458988
Wilpersan <38378542+wilpersan@users.noreply.github.com>
2026-01-17 17:50:10 +0000

    inclusâo arquivos pós `prettier - lint:check : fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # wilpersan_tabnews
+
 Repositório específico para dar seguimento ao curso curso.dev do Felipe Dechamps. Clone do site Tab News.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Olá! Já te disse o quanto você é importante para mim! Se não, você é extremamente importante!</h1>
+  return (
+    <h1>
+      Olá! Já te disse o quanto você é importante para mim! Se não, você é
+      extremamente importante!
+    </h1>
+  );
 }
 
 export default Home;
